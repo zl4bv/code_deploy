a04e40ec9a386cdeb144bccfa21c22491371a4ef
Henry Muru Paenga <meringu@gmail.com>
2016-10-19 16:42:41 +1300

    0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'meringu@gmail.com'
 license          'Apache v2.0'
 description      'Installs and configures CodeDeploy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 supports         'Amazon'
 supports         'rhel'
