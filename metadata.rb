f3fe0a55ee059af991187f4a1af24896162f9305
Ben Vidulich <ben@vidulich.co.nz>
2017-08-01 10:32:31 +1200

    Update cookbook description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'code_deploy'
 maintainer       'Henry Muru Paenga'
 maintainer_email 'meringu@gmail.com'
 license          'Apache-2.0'
-description      'Installs and configures CodeDeploy'
+description      'Installs Amazon CodeDeploy agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 chef_version     '>= 12.5'
 version          '1.0.0'
